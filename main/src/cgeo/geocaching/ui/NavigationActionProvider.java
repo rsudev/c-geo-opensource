fe645f040fbacb97cafbceafa7e1ce7d634fe465
rsudev <rasch@munin-soft.de>
2014-08-05 23:05:42 +0200

    wip

---
+++ main/src/cgeo/geocaching/ui/NavigationActionProvider.java
@@ -0,0 +1,19 @@
+package cgeo.geocaching.ui;
+
+import android.content.Context;
+import android.support.v4.view.ActionProvider;
+import android.view.View;
+
+public class NavigationActionProvider extends ActionProvider {
+
+    public NavigationActionProvider(final Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public View onCreateActionView() {
+        return null;
+    }
+
+}
