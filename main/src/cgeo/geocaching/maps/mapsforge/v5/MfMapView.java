a2fc722f2a2c4c75f0a7e16fa0f09e7dc3672c4c
rsudev <rasch@munin-soft.de>
2015-03-01 23:09:03 +0100

    Livmap first try

--- main/src/cgeo/geocaching/maps/mapsforge/v5/MfMapView.java
+++ main/src/cgeo/geocaching/maps/mapsforge/v5/MfMapView.java
@@ -1,5 +1,10 @@
 package cgeo.geocaching.maps.mapsforge.v5;
 
+import cgeo.geocaching.location.Geopoint;
+import cgeo.geocaching.location.Viewport;
+
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.core.util.MercatorProjection;
 import org.mapsforge.map.android.view.MapView;
 
 import android.content.Context;
@@ -11,14 +16,50 @@ public class MfMapView extends MapView {
         super(context, attributeSet);
     }
 
-    public double getLongitudeSpan() {
-
-        return getModel().mapViewPosition.getMapLimit().getLongitudeSpan();
+    public Viewport getViewport() {
+        final LatLong center = getModel().mapViewPosition.getCenter();
+        return new Viewport(new Geopoint(center.latitude, center.longitude), getLatitudeSpan(), getLongitudeSpan());
     }
 
     public double getLatitudeSpan() {
 
-        return getModel().mapViewPosition.getMapLimit().getLatitudeSpan();
+        double span = 0;
+
+        final long mapSize = MercatorProjection.getMapSize(getModel().mapViewPosition.getZoomLevel(), getModel().displayModel.getTileSize());
+
+        if (getHeight() > 0) {
+
+            final LatLong low = MercatorProjection.fromPixels(0, 0, mapSize);
+            final LatLong high = MercatorProjection.fromPixels(0, getHeight(), mapSize);
+
+            if (low != null && high != null) {
+                span = Math.abs(high.latitude - low.latitude);
+            }
+        }
+
+        return span;
+    }
+
+    public double getLongitudeSpan() {
+
+        double span = 0;
+
+        final long mapSize = MercatorProjection.getMapSize(getModel().mapViewPosition.getZoomLevel(), getModel().displayModel.getTileSize());
+
+        if (getWidth() > 0) {
+            final LatLong low = MercatorProjection.fromPixels(0, 0, mapSize);
+            final LatLong high = MercatorProjection.fromPixels(getWidth(), 0, mapSize);
+
+            if (low != null && high != null) {
+                span = Math.abs(high.longitude - low.longitude);
+            }
+        }
+
+        return span;
+    }
+
+    public int getMapZoomLevel() {
+        return getModel().mapViewPosition.getZoomLevel();
     }
 
 }
