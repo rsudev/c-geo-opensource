bbda87b0ce6f5e50212821118eec2567c57de264
rsudev <rasch@munin-soft.de>
2015-04-20 10:25:20 +0200

    Follow my location

--- main/src/cgeo/geocaching/maps/mapsforge/v5/NewMap.java
+++ main/src/cgeo/geocaching/maps/mapsforge/v5/NewMap.java
@@ -7,7 +7,13 @@ import cgeo.geocaching.activity.ActivityMixin;
 import cgeo.geocaching.enumerations.WaypointType;
 import cgeo.geocaching.location.Geopoint;
 import cgeo.geocaching.maps.CGeoMap.MapMode;
+import cgeo.geocaching.maps.interfaces.GeoPointImpl;
+import cgeo.geocaching.sensors.GeoData;
+import cgeo.geocaching.sensors.GeoDirHandler;
+import cgeo.geocaching.sensors.Sensors;
 import cgeo.geocaching.settings.Settings;
+import cgeo.geocaching.utils.AngleUtils;
+import cgeo.geocaching.utils.Log;
 
 import org.apache.commons.lang3.StringUtils;
 import org.mapsforge.core.model.LatLong;
@@ -19,13 +25,18 @@ import org.mapsforge.map.layer.renderer.TileRendererLayer;
 import org.mapsforge.map.reader.MapFile;
 import org.mapsforge.map.rendertheme.InternalRenderTheme;
 
+import rx.Subscription;
+import rx.subscriptions.Subscriptions;
+
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 
 public class NewMap extends AbstractActionBarActivity {
 
@@ -33,12 +44,33 @@ public class NewMap extends AbstractActionBarActivity {
     private TileCache tileCache;
     private TileRendererLayer tileRendererLayer;
 
+    private String mapTitle;
+    private String geocodeIntent;
+    private Geopoint coordsIntent;
+
+    final private GeoDirHandler geoDirUpdate = new UpdateLoc(this);
+    /**
+     * initialization with an empty subscription to make static code analysis tools more happy
+     */
+    private Subscription resumeSubscription = Subscriptions.empty();
+
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         AndroidGraphicFactory.createInstance(this.getApplication());
+
+        // Get parameters from the intent
+        final Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            geocodeIntent = extras.getString(Intents.EXTRA_GEOCODE);
+            coordsIntent = extras.getParcelable(Intents.EXTRA_COORDS);
+            mapTitle = extras.getString(Intents.EXTRA_TITLE);
+        }
+        if (StringUtils.isBlank(mapTitle)) {
+            mapTitle = res.getString(R.string.map_map);
+        }
 
         ActivityMixin.onCreate(this, true);
 
@@ -67,8 +99,12 @@ public class NewMap extends AbstractActionBarActivity {
     protected void onStart() {
         super.onStart();
 
-        this.mapView.getModel().mapViewPosition.setCenter(new LatLong(52.517037, 13.38886));
-        this.mapView.getModel().mapViewPosition.setZoomLevel((byte) 12);
+        final GeoPointImpl center = Settings.getMapCenter();
+
+        final LatLong myCenter = new LatLong(center.getLatitudeE6() / 1.0e6, center.getLongitudeE6() / 1.0e6);
+
+        this.mapView.getModel().mapViewPosition.setCenter(myCenter);
+        this.mapView.getModel().mapViewPosition.setZoomLevel((byte) Settings.getMapZoom(MapMode.SINGLE));
 
         // tile renderer layer using internal render theme
         this.tileRendererLayer = new TileRendererLayer(tileCache, new MapFile(NewMap.getMapFile()),
@@ -77,11 +113,15 @@ public class NewMap extends AbstractActionBarActivity {
 
         // only once a layer is associated with a mapView the rendering starts
         this.mapView.getLayerManager().getLayers().add(tileRendererLayer);
+
+        resumeSubscription = Subscriptions.from(geoDirUpdate.start(GeoDirHandler.UPDATE_GEODIR));
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+
+        resumeSubscription.unsubscribe();
 
         this.mapView.getLayerManager().getLayers().remove(this.tileRendererLayer);
         this.tileRendererLayer.onDestroy();
@@ -95,6 +135,18 @@ public class NewMap extends AbstractActionBarActivity {
         this.mapView.getModel().mapViewPosition.destroy();
         this.mapView.destroy();
         AndroidResourceBitmap.clearResourceBitmaps();
+    }
+
+    private void centerMap(final Geopoint geopoint) {
+        this.mapView.getModel().mapViewPosition.setCenter(new LatLong(geopoint.getLatitude(), geopoint.getLongitude()));
+    }
+
+    public Location getCoordinates() {
+        final LatLong center = mapView.getModel().mapViewPosition.getCenter();
+        final Location loc = new Location("newmap");
+        loc.setLatitude(center.latitude);
+        loc.setLongitude(center.longitude);
+        return loc;
     }
 
     private static File getMapFile() {
@@ -122,6 +174,106 @@ public class NewMap extends AbstractActionBarActivity {
         mapIntent.putExtra(Intents.EXTRA_GEOCODE, geocode);
         mapIntent.putExtra(Intents.EXTRA_TITLE, geocode);
         fromActivity.startActivity(mapIntent);
+    }
+
+    // class: update location
+    private static class UpdateLoc extends GeoDirHandler {
+        // use the following constants for fine tuning - find good compromise between smooth updates and as less updates as possible
+
+        // minimum time in milliseconds between position overlay updates
+        private static final long MIN_UPDATE_INTERVAL = 500;
+        // minimum change of heading in grad for position overlay update
+        private static final float MIN_HEADING_DELTA = 15f;
+        // minimum change of location in fraction of map width/height (whatever is smaller) for position overlay update
+        private static final float MIN_LOCATION_DELTA = 0.01f;
+
+        Location currentLocation = Sensors.getInstance().currentGeo();
+        float currentHeading;
+
+        private long timeLastPositionOverlayCalculation = 0;
+        /**
+         * weak reference to the outer class
+         */
+        private final WeakReference<NewMap> mapRef;
+
+        public UpdateLoc(final NewMap map) {
+            mapRef = new WeakReference<>(map);
+        }
+
+        @Override
+        public void updateGeoDir(final GeoData geo, final float dir) {
+            currentLocation = geo;
+            currentHeading = AngleUtils.getDirectionNow(dir);
+            repaintPositionOverlay();
+        }
+
+        /**
+         * Repaint position overlay but only with a max frequency and if position or heading changes sufficiently.
+         */
+        void repaintPositionOverlay() {
+            final long currentTimeMillis = System.currentTimeMillis();
+            if (currentTimeMillis > timeLastPositionOverlayCalculation + MIN_UPDATE_INTERVAL) {
+                timeLastPositionOverlayCalculation = currentTimeMillis;
+
+                try {
+                    final NewMap map = mapRef.get();
+                    if (map != null) {
+                        final boolean needsRepaintForDistanceOrAccuracy = needsRepaintForDistanceOrAccuracy();
+                        final boolean needsRepaintForHeading = needsRepaintForHeading();
+
+                        if (needsRepaintForDistanceOrAccuracy) {
+                            //                            if (CGeoMap.followMyLocation) {
+                            map.centerMap(new Geopoint(currentLocation));
+                            //                            }
+                        }
+
+                        //                        if (needsRepaintForDistanceOrAccuracy || needsRepaintForHeading) {
+                        //
+                        //                            map.overlayPositionAndScale.setCoordinates(currentLocation);
+                        //                            map.overlayPositionAndScale.setHeading(currentHeading);
+                        //                            map.mapView.repaintRequired(map.overlayPositionAndScale);
+                        //                        }
+                    }
+                } catch (final RuntimeException e) {
+                    Log.w("Failed to update location", e);
+                }
+            }
+        }
+
+        boolean needsRepaintForHeading() {
+            //            final NewMap map = mapRef.get();
+            //            if (map == null) {
+            return false;
+            //            }
+            //            return Math.abs(AngleUtils.difference(currentHeading, map.overlayPositionAndScale.getHeading())) > MIN_HEADING_DELTA;
+        }
+
+        boolean needsRepaintForDistanceOrAccuracy() {
+            final NewMap map = mapRef.get();
+            if (map == null) {
+                return false;
+            }
+            final Location lastLocation = map.getCoordinates();
+
+            float dist = Float.MAX_VALUE;
+            if (lastLocation != null) {
+                if (lastLocation.getAccuracy() != currentLocation.getAccuracy()) {
+                    return true;
+                }
+                dist = currentLocation.distanceTo(lastLocation);
+            }
+
+            final float[] mapDimension = new float[1];
+            if (map.mapView.getWidth() < map.mapView.getHeight()) {
+                final double span = map.mapView.getLongitudeSpan() / 1e6;
+                Location.distanceBetween(currentLocation.getLatitude(), currentLocation.getLongitude(), currentLocation.getLatitude(), currentLocation.getLongitude() + span, mapDimension);
+            } else {
+                final double span = map.mapView.getLatitudeSpan() / 1e6;
+                Location.distanceBetween(currentLocation.getLatitude(), currentLocation.getLongitude(), currentLocation.getLatitude() + span, currentLocation.getLongitude(), mapDimension);
+            }
+
+            return dist > (mapDimension[0] * MIN_LOCATION_DELTA);
+        }
     }
 
 }
